83bb38988cdcb2582139e4adad64e09c29d18fa8
MBFouad <m7md.bahaa1@gmail.com>
2022-02-19 05:17:11 +0200

    finsih todo without storage

--- public/js/todo.js
+++ public/js/todo.js
@@ -3,6 +3,7 @@ class Todo {
         this.id = id;
         this.body = body;
         this.completed = false;
+        this.editing = false;
     }
 }
 
@@ -10,12 +11,29 @@ window.todos = function () {
     return {
         newTodo: '',
         todos: [],
+        filter: 'all',
+        get allCompleted() {
+            return this.todos.length == this.completed.length;
+        },
         get active() {
             return this.todos.filter(todo => !todo.completed);
         },
+        get completed() {
+            return this.todos.filter(todo => todo.completed);
+        },
+        get filterTodos() {
+            return {
+                all: this.todos,
+                active: this.active,
+                completed: this.completed
+            }[this.filter];
+        },
+        setFilter(newFilter) {
+            this.filter = newFilter;
+        },
         addNewTodo() {
             if (this.newTodo.trim().length) {
-                this.todos.push((new Todo(this.todos.length + 1, this.newTodo)));
+                this.todos.push((new Todo(Date.now(), this.newTodo)));
                 this.clearInput();
             }
         },
@@ -27,7 +45,34 @@ window.todos = function () {
         },
         completeTodo(todo) {
             todo.completed = !todo.completed;
+        },
+        clearCompletedTodo() {
+            this.todos = this.active;
+        },
+        editTodo(todo) {
+            todo.editing = true;
+            todo.bodyBeforeEdit = todo.body;
+        },
+        finishEditTodo(todo) {
+            if (todo.body.trim() === '') {
+                this.destroyTodo(todo.id);
+            }
+            todo.editing = false;
+        },
+        cancelEditTodo(todo) {
+            todo.body = todo.bodyBeforeEdit;
+            delete todo.bodyBeforeEdit;
+            this.finishEditTodo(todo);
+        },
+        pluraize(word, length) {
+            if (length > 1) {
+                return word + 's';
+            }
+            return word
+        },
+        toggleTodos() {
+            let allCompleted = this.allCompleted;
+            this.todos.forEach(todo => todo.completed = !allCompleted);
         }
-
     }
 };
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,13 +22,21 @@
     </header>
     <!-- This section should be hidden by default and shown when there are todos -->
     <section class="main" x-show="todos.length">
-        <input id="toggle-all" class="toggle-all" type="checkbox">
+        <input
+            id="toggle-all"
+            class="toggle-all"
+            type="checkbox"
+            x-model="allCompleted"
+            @click="toggleTodos()"
+        >
         <label for="toggle-all">Mark all as complete</label>
         <ul class="todo-list">
             <!-- These are here just to show the structure of the list items -->
             <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-            <template x-for="todo in todos" :key="todo.id">
-                <li :class="{completed:todo.completed}">
+            <template x-for="todo in filterTodos" :key="todo.id">
+                <li
+                    :class="{completed:todo.completed,editing:todo.editing}"
+                >
                     <div class="view">
                         <input
                             class="toggle"
@@ -37,10 +45,18 @@
                             x-model="todo.completed"
                         >
 
-                        <label x-text="todo.body"></label>
+                        <label
+                            x-text="todo.body"
+                            @dblclick="editTodo(todo)"
+                        ></label>
                         <button class="destroy" @click="destroyTodo(todo.id)"></button>
                     </div>
-                    <input class="edit" value="Create a TodoMVC template">
+                    <input
+                        class="edit"
+                        x-model="todo.body"
+                        @keyup.enter="finishEditTodo(todo)"
+                        @keyup.escape="cancelEditTodo(todo)"
+                    >
                 </li>
             </template>
         </ul>
@@ -48,21 +64,26 @@
     <!-- This footer should be hidden by default and shown when there are todos -->
     <footer class="footer" x-show="todos.length">
         <!-- This should be `0 items left` by default -->
-        <span class="todo-count"><strong x-text="active.length">0</strong> item left</span>
+        <span class="todo-count"><strong x-text="active.length">0</strong>
+            <span
+                x-text="pluraize('item',active.length)"
+            ></span>
+            left
+        </span>
         <!-- Remove this if you don't implement routing -->
         <ul class="filters">
             <li>
-                <a class="selected" href="#/">All</a>
+                <a :class="{selected:this.filter=='all'}" href="#/" @click.prevent="setFilter('all')">All</a>
             </li>
             <li>
-                <a href="#/active">Active</a>
+                <a href="#/active" @click.prevent="setFilter('active')">Active</a>
             </li>
             <li>
-                <a href="#/completed">Completed</a>
+                <a href="#/completed" @click.prevent="setFilter('completed')">Completed</a>
             </li>
         </ul>
         <!-- Hidden if no completed items are left ↓ -->
-        <button class="clear-completed">Clear completed</button>
+        <button x-show="completed.length" @click="clearCompletedTodo()" class="clear-completed">Clear completed</button>
     </footer>
 </section>
 <footer class="info">
